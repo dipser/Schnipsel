c483beb2a1f97f7fb47b074c2904e255dcb5b424
Aurel Hermand <github@devone.de>
2014-04-14 10:28:13 +0200

    Create localStorage-NurEinmalAmTag.js

---
+++ localStorage-NurEinmalAmTag.js
@@ -0,0 +1,16 @@
+
+var currentDate = new Date();
+var day = currentDate.getDate();
+var month = currentDate.getMonth() + 1;
+var year = currentDate.getFullYear();
+var today = year+''+month+''+day;
+if (localStorage.getItem('today') === null) {
+  // Set localstorage timestamp
+  localStorage.setItem('today', today);
+} else {
+  // Clear localstorage if not today
+  if (localStorage.getItem('today') != today) {
+    localStorage.clear();
+    localStorage.setItem('today', today);
+  }
+}
