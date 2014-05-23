59b2f0e7ede8dc06f5d51429adb9b37cffed994c
Aurel Hermand <github@devone.de>
2014-05-23 23:48:18 +0200

    Update Dokument.php

--- webEdition/Dokument.php
+++ webEdition/Dokument.php
@@ -3,12 +3,12 @@
 
 include_once $_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we.inc.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we_classes/we_webEditionDocument.inc.php';
- 
-
 $doc = new we_webEditionDocument();
 $doc->initByID(9);
- 
-$Headline = $doc->getElement('Headline');
- 
+$doc->getElement('Headline');
+$doc->setElement('Headline', 'Wert');
+$doc->we_save();
+$doc->we_publish();
+
 
 ?>
