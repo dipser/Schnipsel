b7aef2d0c25f93a000308e18f4c45cb4dc9761bc
Aurel Hermand <github@devone.de>
2014-05-23 23:30:53 +0200

    Create Block.php

---
+++ webEdition/Block.php
@@ -0,0 +1,9 @@
+<we:block name="test" start="5">
+  <we:ifPosition position="1" type="block" reference="start">
+    <we:textarea name="content">Dies ist Inhalt 1</we:textarea>
+  </we:ifPosition>
+
+  <we:ifPosition position="2" type="block" reference="start">
+    <we:textarea name="content">Dies ist Inhalt 2</we:textarea>
+  </we:ifPosition>
+</we:block>
