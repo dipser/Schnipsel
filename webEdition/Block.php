71f1eb35895f55fb111567467f2397e12ac9c24c
Aurel Hermand <github@devone.de>
2014-05-24 01:23:35 +0200

    Update Block.php

--- webEdition/Block.php
+++ webEdition/Block.php
@@ -7,3 +7,20 @@
     <we:textarea name="content">Dies ist Inhalt 2</we:textarea>
   </we:ifPosition>
 </we:block>
+
+
+
+    $em=$GLOBALS['we_editmode'];
+    $GLOBALS['we_editmode']=false;
+    $block=we_tag('block',array("name"=>"test"));
+    while(we_condition_tag_block($block)){
+      $innerBlock=we_tag('block',array("name"=>"inner"));
+      while(we_condition_tag_block($innerBlock)){
+        $inhalt=we_tag('input',array("type"=>"text"));
+      }
+    }
+    $GLOBALS['we_editmode']=$em;
+
+
+so bist du auch weitestgehend auf der sicheren Seite bei Updates.
+Du solltest noch $block bzw. $innerBlock auf !=false prüfen.
