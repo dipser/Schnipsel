4bf3bdfcf987773867d7252206e4c27afa08eca3
Aurel Hermand <github@devone.de>
2014-05-24 16:57:33 +0200

    Update DB.php

--- webEdition/DB.php
+++ webEdition/DB.php
@@ -6,9 +6,11 @@ $db = $GLOBALS['DB_WE'];
 $db->escape($unsafe_variable); // Sanitizing data
 $db->query("SELECT Path FROM " . FILE_TABLE . " WHERE IsFolder=0 ORDER By ID");
 print "Anzahl Zeilen: " . $db->num_rows() . "<br>";
+echo $db->num_rows();
 while ($db->next_record()) {
 	print $db->f("Path") . "<br>";
 }
+
 
 
 ?>
