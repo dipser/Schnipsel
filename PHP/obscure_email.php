9f522bb952cdda4e17afffa43f413b46e118f057
Aurel Hermand <github@devone.de>
2014-05-27 23:46:17 +0200

    ent-einrücken

--- PHP/obscure_email.php
+++ PHP/obscure_email.php
@@ -1,9 +1,11 @@
 <?php
-	function obscure_email($e) {
-		$output = '';
-		for ($i = 0; $i < strlen($e); $i++) {
-			$output .= '&#'.ord($e[$i]).';';
-		}
-		return $output;
+
+function obscure_email($e) {
+	$output = '';
+	for ($i = 0; $i < strlen($e); $i++) {
+		$output .= '&#'.ord($e[$i]).';';
 	}
+	return $output;
+}
+
 ?>
