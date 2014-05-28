9fa867ef7221b0a4c94a014e9ebb4c46c099231f
Aurel Hermand <github@devone.de>
2014-05-28 20:51:18 +0200

    Update Variablen.php

--- webEdition/Variablen.php
+++ webEdition/Variablen.php
@@ -10,6 +10,9 @@ $tinyAttr = "theme_advanced_blockformats:'p,h2,h3,h4'";
 $anzahl = count(unserialize($GLOBALS['we_doc']->getElement('meinBlock')));
 
 
+$doclang = $GLOBALS['we_doc']->Language;
+
+
 // Anzahl innerhalb eines listviews
 //<we:listviewRows /> 
 <? echo $lv->anz_all; ?>
