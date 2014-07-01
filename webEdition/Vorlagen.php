4656fa6e9455b006824bf8005a61a03685de8cb8
Aurel <github@devone.de>
2014-07-01 17:55:53 +0200

    no message

---
+++ webEdition/Vorlagen.php
@@ -0,0 +1,42 @@
+<?php
+	
+	$newTemplateFilename = 'perphp';
+	$templateParentID = 0;
+	$templateCode = 'Hallo Welt.';
+	
+	
+	// ID Anhand des Vorlagen-Pfads ermitteln
+	$ID = path_to_id(id_to_path($templateParentID).$newTemplateFilename.'.tmpl', TEMPLATES_TABLE);
+	$Filename = f('SELECT Filename FROM '.TEMPLATES_TABLE.' WHERE ID='.$ID, "Filename", $GLOBALS['DB_WE']);
+	echo $ID.' '.$Filename;
+	
+	
+	// Vorlage erstellen
+	if ($ID == 0) {
+		$tmpl = new we_template();
+		$tmpl->we_new();
+		$tmpl->CreationDate = time();
+		$tmpl->ID = 0;
+		$tmpl->OldPath = "";
+		$tmpl->Extension = ".tmpl";
+		$tmpl->Filename = $newTemplateFilename;
+		$tmpl->Text = $tmpl->Filename . $tmpl->Extension;
+		$tmpl->setParentID($templateParentID);
+		$tmpl->Path = $tmpl->ParentPath . ($templateParentID ? "/" : "") . $tmpl->Text;
+		$tmpl->OldPath = $tmpl->Path;
+		$tmpl->setElement('data', $templateCode, "txt");
+		$tmpl->we_save();
+		$tmpl->we_publish();
+		$tmpl->setElement('Charset', 'UTF-8');
+		$newTemplateID = $tmpl->ID;
+	}
+	
+	
+	// Template bearbeiten
+	$tmpl = new we_template( );
+	$tmpl->initByID( $ID, TEMPLATES_TABLE );
+	$tmpl->setElement('data', '<?php echo "Hallo Welt."; ?>', "txt");
+	$tmpl->we_save();
+	$tmpl->we_publish();
+	
+?>
