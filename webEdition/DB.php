e8b860facbc5cfeb2a1a8b1087bd8479a65f0c2a
Aurel Hermand <github@devone.de>
2014-07-15 11:18:01 +0200

    affected_rows

--- webEdition/DB.php
+++ webEdition/DB.php
@@ -5,8 +5,8 @@
 $db = $GLOBALS['DB_WE'];
 $db->escape($unsafe_variable); // Sanitizing data
 $db->query("SELECT Path FROM " . FILE_TABLE . " WHERE IsFolder=0 ORDER By ID");
-print "Anzahl Zeilen: " . $db->num_rows() . "<br>";
-echo $db->num_rows();
+echo 'Anzahl Zeilen: ' . $db->num_rows() . '<br>';
+//echo 'Anzahl geänderter Zeilen: ' . $db->affected_rows() . '<br>';
 while ($db->next_record()) {
 	print $db->f("Path") . "<br>";
 }
