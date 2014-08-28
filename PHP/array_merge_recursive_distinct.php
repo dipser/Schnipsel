faad7289e5fd055f63d368d82c888ca10fe4007a
Aurel Hermand <github@devone.de>
2014-08-28 16:13:22 +0200

    Update array_merge_recursive_distinct.php

--- PHP/array_merge_recursive_distinct.php
+++ PHP/array_merge_recursive_distinct.php
@@ -1,13 +1,13 @@
-function &array_merge_recursive_distinct(array &$array1, &$array2 = null)
-{
+function &array_merge_recursive_distinct(array &$array1, &$array2 = null) {
   $merged = $array1;
-  
-  if (is_array($array2))
-    foreach ($array2 as $key => $val)
-      if (is_array($array2[$key]))
+  if (is_array($array2)) {
+    foreach ($array2 as $key => $val) {
+      if (is_array($array2[$key])) {
         $merged[$key] = is_array($merged[$key]) ? array_merge_recursive_distinct($merged[$key], $array2[$key]) : $array2[$key];
-      else
+      } else {
         $merged[$key] = $val;
-  
+			}
+		}
+	}
   return $merged;
 }
