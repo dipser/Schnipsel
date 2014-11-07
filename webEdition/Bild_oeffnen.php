6a53f50ca78365c143465d7684c44d67b42394e9
Aurel Hermand <github@devone.de>
2014-11-07 22:21:23 +0100

    Update Bild_oeffnen.php

--- webEdition/Bild_oeffnen.php
+++ webEdition/Bild_oeffnen.php
@@ -10,4 +10,40 @@ $img->initByID( 5285 );
 p_r($img);
 //$img->we_save();
 
+
+
+
+
+function uploadFile($file,$parentid)
+{   
+   // übergeben wird ein $_FILE-Objekt, also z.B. $_FILE['upload']
+
+   // Extension ermitteln
+   $pathinfo = pathinfo($file['name']);
+   $ext = strtolower($pathinfo['extension']);
+   
+   $path = $file['tmp_name'];
+   $filedata = file_get_contents($path);
+   
+   // Filename
+   $filename = md5(userVal('id').time()); // wird in meinem Fall verschlüsselt
+   
+   if(isImage($file)) $doc = new we_imageDocument();
+   else $doc = new we_otherDocument();
+   
+   $doc->Filename = $filename;
+   $doc->Extension = '.'.$ext;
+   $doc->Text = $doc->Filename.$doc->Extension;
+   $doc->Path = id_to_path($parentid) .'/'. $doc->Text;
+   $fileserverpath = $_SERVER['DOCUMENT_ROOT'].$doc->Path;
+   file_put_contents($fileserverpath, $filedata);
+   $doc->setParentID($parentid);
+   $doc->setElement("filesize", filesize($fileserverpath), "attrib");
+   $doc->setElement("type", getContentTypeFromFile($path), "attrib");
+   $doc->Table = FILE_TABLE;
+   $doc->DocChanged = true;
+   $doc->we_save();
+   return $doc->ID;
+}
+
 ?>
