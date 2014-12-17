fa584463c222cdae8c48f8b7e8cc357cffcc9764
Aurel Hermand <github@devone.de>
2014-12-18 00:44:50 +0100

    Create js.ts

---
+++ Typo3/ts/js.ts
@@ -0,0 +1,6 @@
+page.includeJSFooter.jquery = /fileadmin/.../jquery.js
+page.includeJSFooter.bootstrap = /fileadmin/.../bootstrap.js
+[browser = msie] and [version= <9]
+page.includeJSFooter.html5shiv = /fileadmin/.../html5shiv.min.js
+page.includeJSFooter.respond = /fileadmin/.../respon.min.js
+[global]
