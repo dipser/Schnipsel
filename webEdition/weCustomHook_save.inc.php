d2f54d0969115c3f173b596b78c86170c65b4c16
Aurel Hermand <github@devone.de>
2015-04-09 13:23:33 +0200

    Update weCustomHook_save.inc.php

--- webEdition/weCustomHook_save.inc.php
+++ webEdition/weCustomHook_save.inc.php
@@ -23,3 +23,49 @@ function weCustomHook_save($param){
 	//$hookHandler->setErrorString('I don\'t like you! Go away.');
 }
 ?>
+
+
+
+
+<?php
+// CSS & JS Minimierung
+// Code by Andreas Witt - http://qa.webedition.org/tracker/view.php?id=9686
+function weCustomHook_save($param) {
+    $savedObj=$param[0];
+    
+    switch(get_class($savedObj)){
+        
+        case 'we_textDocument':
+            switch($savedObj->ContentType){
+                case 'text/js':
+                    /**
+                    * https://code.google.com/p/minify/source/browse/min/lib/JSMin.php
+                    */
+                    require $_SERVER['DOCUMENT_ROOT'] . id_to_path(1477); //sharedObject/minify/lib/JSMin.php
+
+                    $jsFile = $_SERVER['DOCUMENT_ROOT'] . id_to_path($savedObj->ID);
+                    $output = JSMin::minify(file_get_contents($jsFile));
+                    file_put_contents($jsFile, $output);
+                    break;
+                case 'text/css':
+                    /**
+                    * https://code.google.com/p/cssmin/
+                    */
+                    require $_SERVER['DOCUMENT_ROOT'] . id_to_path(1531); //sharedObject/minify/lib/cssmin-3.0.1.php
+                    
+                    $cssFile = $_SERVER['DOCUMENT_ROOT'] . id_to_path($savedObj->ID);
+                    $output = CssMin::minify(file_get_contents($cssFile));
+                    file_put_contents($cssFile, $output);
+                    break;
+                default:
+                    break;
+            }
+
+            break;
+        
+        default: //no default document class
+            break;
+    }
+    
+}
+?>
