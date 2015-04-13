f770ee9061d29863f0e15097012dbe484a76b1e5
Aurel Hermand <github@devone.de>
2015-04-14 00:49:15 +0200

    Create pdo.php

---
+++ PHP/pdo.php
@@ -0,0 +1,25 @@
+<?php
+
+// Datenbank Einstellung
+define('DBHOST', 'localhost');
+define('DBPORT', 8889);
+define('DBNAME', 'db');
+define('DBUSER', 'root');
+define('DBPASS', 'root');
+
+// PDO Verbindung aufbauen
+$GLOBALS['pdo'] = $pdo = new PDO('mysql:host='.DBHOST.';port='.DBPORT.';dbname='.DBNAME, DBUSER, DBPASS, array( PDO::ATTR_PERSISTENT => false));
+
+
+// Listenausgabe
+$sql = 'SELECT * FROM tablename WHERE userid = :userid';
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':userid', $userid, PDO::PARAM_INT);
+$stmt->execute();
+while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+    echo $row->id.'<br />';
+}
+
+
+
+?>
