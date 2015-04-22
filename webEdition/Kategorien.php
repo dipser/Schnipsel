1c8067e773bbeeb4583f8c04be841dce4fb5c3e9
Aurel Hermand <github@devone.de>
2015-04-22 11:26:13 +0200

    Update Kategorien.php

--- webEdition/Kategorien.php
+++ webEdition/Kategorien.php
@@ -8,3 +8,32 @@ while($GLOBALS['DB_WE']->next_record())
     echo '<option'.(($MyCategories == $Path) ? ' selected="selected"' : '').'>'.$Path.'</option>';
 }
 ?>
+
+
+
+<div>
+	<we:listview type="category" name="WasAuchImmer" parentid="1">
+		<we:repeat>
+			<we:field name="ID" to="global" /><?php $pID = $GLOBALS['ID']; ?>
+			<we:field name="Category" to="global" /><?php $pCategory = $GLOBALS['Category']; ?>
+			<we:field name="Title" to="global" /><?php $pTitle = $GLOBALS['Title']; ?>
+			<div class="group" style="margin-bottom:10px;">
+				<b><?=( strlen($pTitle)>0 ? $pTitle : $pCategory )?></b><br />
+				<we:listview type="category" parentid="$pID">
+					<we:repeat>
+						<we:field name="ID" to="global" /><?php $ID = $GLOBALS['ID']; ?>
+						<we:field name="Category" to="global" /><?php $Category = $GLOBALS['Category']; ?>
+						<we:field name="Title" to="global" /><?php $Title = $GLOBALS['Title']; ?>
+						<div>
+							<we:input type="checkbox" name="x_$ID" id="x_$ID" title="Kategorie-ID: $ID" />
+							<label for="x_<?=$ID?>" style="margin-left:5px;"><?=( strlen($Title)>0 ? $Title : $Category )?></label>
+							<we:ifNotEmpty match="x_$ID">
+								...
+							</we:ifNotEmpty>
+						</div>
+					</we:repeat>
+				</we:listview>
+			</div>
+		</we:repeat>
+	</we:listview>
+</div>
