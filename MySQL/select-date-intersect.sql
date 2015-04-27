6ff3b0bbf789e50dce1b950ed63f857e5cd968b7
Aurel Hermand <github@devone.de>
2015-04-27 12:52:12 +0200

    Update select-date-intersect.sql

--- MySQL/select-date-intersect.sql
+++ MySQL/select-date-intersect.sql
@@ -1,5 +1,18 @@
-SELECT * FROM `s2_r_events` WHERE section_id LIKE '32-%' AND (
-(`start_date` BETWEEN '2015-04-27 00:00:00' AND '2015-05-04 00:00:00') OR
-(`start_date` < '2015-04-27 00:00:00' AND `end_date` > '2015-05-04 00:00:00') OR
-(`start_date` < '2015-04-27 00:00:00' AND `end_date` < '2015-05-04 00:00:00' AND `end_date` > '2015-04-27 00:00:00') OR
-(`start_date` > '2015-04-27 00:00:00' AND `end_date` > '2015-05-04 00:00:00') AND `start_date` < '2015-05-04 00:00:00')
+Beispiel:
+SELECT * FROM `s2_r_events` WHERE
+(
+	(`start_date` BETWEEN '2015-04-27 00:00:00' AND '2015-05-04 00:00:00') OR
+	(`start_date` < '2015-04-27 00:00:00' AND `end_date` > '2015-05-04 00:00:00') OR
+	(`start_date` < '2015-04-27 00:00:00' AND `end_date` < '2015-05-04 00:00:00' AND `end_date` > '2015-04-27 00:00:00') OR
+	(`start_date` > '2015-04-27 00:00:00' AND `end_date` > '2015-05-04 00:00:00' AND `start_date` < '2015-05-04 00:00:00')
+)
+
+
+PDO:
+SELECT * FROM `table` WHERE 
+(
+	(`start_date` BETWEEN :start_date AND :end_date) OR
+	(`start_date` < :start_date AND `end_date` > :end_date) OR
+	(`start_date` < :start_date AND `end_date` < :end_date AND `end_date` > :start_date) OR
+	(`start_date` > :start_date AND `end_date` > :end_date AND `start_date` < :end_date)
+)
