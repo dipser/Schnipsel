d763f63ebbdf7a390bae48ce146db24d70580d64
Aurel Hermand <github@devone.de>
2015-08-05 09:01:32 +0200

    Update checkbox.php

--- webEdition/checkbox.php
+++ webEdition/checkbox.php
@@ -9,8 +9,8 @@
 	<we:input type="checkbox" name="x" value="1" />
 <we:else />
 	<we:ifVar name="x" match="1">
-	true
-<we:else />
-	false
-</we:ifVar>
+		true
+	<we:else />
+		false
+	</we:ifVar>
 </we:ifEditmode>
