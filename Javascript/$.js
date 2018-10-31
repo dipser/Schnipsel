e249d2aa1b8511a920a965bbec05143e2fe78dd4
Aurel Hermand <github@devone.de>
2018-10-31 10:48:33 +0100

    Update $.js

--- Javascript/$.js
+++ Javascript/$.js
@@ -1,4 +1,4 @@
-window.$q = document.querySelector.bind(document);
-window.$all = document.querySelectorAll.bind(document);
+window.$q = document.querySelector.bind(document); // Same as $(selector, [startNode]) in modern browser commandline
+window.$all = document.querySelectorAll.bind(document); // Same as $$(selector, [startNode]) in modern browser commandline
 
 // The dollar sign should only be used in frameworks.
