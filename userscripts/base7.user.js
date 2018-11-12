e7146e811ec56348eeed1bc93475cd38657133ca
Aurel Hermand <github@devone.de>
2018-11-13 00:26:05 +0100

    Update base7.user.js

--- userscripts/base7.user.js
+++ userscripts/base7.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Base7
 // @namespace    http://vegvisir.de/
-// @version      1.7.5
+// @version      1.7.6
 // @updateURL    https://raw.githubusercontent.com/dipser/Schnipsel/master/userscripts/base7.user.js
 // @description  Trying to make Base7 better!
 // @author       Aurelian Hermand
@@ -24,7 +24,7 @@ function matchRuleShort(str, rule) {
 
     // Allgemeines Anzeigeelement
     var disablebrowserpagetext = '@page { size: auto; margin: 0; }';// html {background-color: #FFFFFF;margin: 0px; } body {margin: 10mm 15mm 10mm 15mm; }';
-    var printmediacss = '@media print { body { margin:0; padding: 20px 10px !important; } .no-print, .no-print * { display: none !important; } }';
+    var printmediacss = '@media print { body { margin:0; padding: 20px 30px !important; } .no-print, .no-print * { display: none !important; } }';
     var printstyle = '<style>' + disablebrowserpagetext + printmediacss + '</style>';
     $('body').append(printstyle + '<div id="monkeybox" class="no-print">-</div>');
     $("#monkeybox").css("position", "fixed").css("top", 0).css("right", 0).css('display', 'none');
