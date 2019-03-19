fdca17c1d0a7942ed6c2cf3ff476316a162839b2
Aurel Hermand <github@devone.de>
2019-03-19 12:43:17 +0100

    Update c_l.php

--- PHP/c_l.php
+++ PHP/c_l.php
@@ -1,6 +1,7 @@
-<?
+<?php
 // Logs an array like $we_doc to firebug etc.
-function c_l($r) {
-    echo '<script>var console=console||{"log":function(){}};console.log('.json_encode($r).');</script>';
+function c_l() {
+    echo '<script>var console=console||{"log":function(){}};console.log('.json_encode(func_get_args()).');</script>';
 }
+c_l('Get', $_GET, 'Cookie', $_COOKIE, 'Session', $_SESSION);
 ?>
