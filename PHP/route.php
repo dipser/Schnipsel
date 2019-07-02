f6314a467e4f8e8ccb14cfa53127b5462b9151e6
Aurel Hermand <github@devone.de>
2019-07-03 00:18:52 +0200

    Update route.php

--- PHP/route.php
+++ PHP/route.php
@@ -5,5 +5,5 @@ define('ABSPATH', dirname(__FILE__));
 // ...
 
 // Route
-$request_path = str_replace(ABSPATH, '', $_SERVER['DOCUMENT_ROOT'].$_SERVER['REQUEST_URI']);
+$request_path = str_replace(ABSPATH, '', rtrim($_SERVER['DOCUMENT_ROOT'], '/').$_SERVER['REQUEST_URI']);
 $route = parse_url($request_path, PHP_URL_PATH);
