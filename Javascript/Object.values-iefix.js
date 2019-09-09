3b7461e6e9f8016e20b644b3f6c6c0dc1ea067ec
Aurel Hermand <github@devone.de>
2019-09-09 11:50:39 +0200

    Update Object.values-iefix.js

--- Javascript/Object.values-iefix.js
+++ Javascript/Object.values-iefix.js
@@ -1,3 +1,3 @@
-// Object to array
+// Object to array (ES8, 2017)
 if (!Object.values) { Object.values = function (obj) { return Object.keys(obj).map(function(key) { return obj[key]; }); } } // Internet Explorer fix
-var values = Object.values( {a:1, b:2} ); // ES8
+var values = Object.values( {a:1, b:2} ); // => [1,2]
