d2f10f7b7f9af56cfb722bcd0040b61b224c463f
Aurel Hermand <github@devone.de>
2020-03-13 13:29:21 +0100

    Update mdarray_only.php

--- PHP/mdarray_only.php
+++ PHP/mdarray_only.php
@@ -1,5 +1,9 @@
 <?php
 
+// Mulidimentional array "only". Keeps only specified columns.
+// Example for only keeping "id": 
+//      mdarray_only([['id'=>1, 'name'=>1], ['id'=>2, 'name'=>2], ...], ['id']);
+//      Result: [['id'=>1], ['id'=>2], ...]
 function mdarray_only($array, $only_columns = []) {
     $array = (array) $array;
     array_walk($array, function(&$row, $key) use ($only_columns) { // All rows
