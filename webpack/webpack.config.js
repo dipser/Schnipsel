c6ca98d391a5540b84b3a946a3a73cd5f2c8aa25
Aurel Hermand <github@devone.de>
2020-08-06 10:35:21 +0200

    Create webpack.config.js

---
+++ webpack/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    //watch: true,
+    watchOptions: {
+        ignored: /node_modules/,
+        aggregateTimeout: 200, // after build
+        poll: 1000 // every
+    },
+    entry: {
+        main: ['./assets/js/main.js', './assets/css/main.scss']
+    },
+    output: {
+        path: path.resolve(__dirname, 'assets'), 
+        filename: 'js/[name].min.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [],
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'css',
+                            name: '[name].min.css'
+                        }
+                    },
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+};
