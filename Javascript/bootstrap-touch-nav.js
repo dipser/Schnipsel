c1b77e06fe86406631a260f0aab463b07f7f847d
Aurel Hermand <github@devone.de>
2020-10-27 18:02:58 +0100

    Update bootstrap-touch-nav.js

--- Javascript/bootstrap-touch-nav.js
+++ Javascript/bootstrap-touch-nav.js
@@ -1,7 +1,73 @@
 /**
- * Bootstrap Touch Nav: Touble Tap to open dropdown
+ * Bootstrap Touch Nav: double Tap to open dropdown
  */
-document.getElementById('#site-nav .dropdown-toggle').addEventListener('touchstart', function() {
-    // if input needs double tap 
-    this.focus();
+
+window.touched_start = 0;
+window.touched = null;
+document.addEventListener('DOMContentLoaded', function(event) {
+
+    var bsdropdowns = document.querySelectorAll('.navbar .menu-item.dropdown');
+    bsdropdowns.forEach((item, index) => {
+        
+        item.addEventListener('mouseover', function(e) {
+            menu_dropdown_show(item);
+        });
+        item.addEventListener('mouseout', function(e) {
+            menu_dropdown_hide(item);
+        });
+
+        item.addEventListener('touchstart', function(e) {
+            window.touched_start = +new Date;
+            console.log('touched start');
+        });
+        item.addEventListener('touchend', function(e) {
+            console.log('touched end');
+            let diff = (+new Date) - window.touched_start;
+
+
+            // if item touched again -> goto link
+            if ( window.touched != null && window.touched == item && item.contains(e.target) ) {
+                // do nothing
+            }
+            // if item touched first time
+            else {
+
+                // if item touched -> open item and close prev touched item
+                if ( item.contains(e.target) ) {
+                    e.preventDefault();
+                    if (window.touched != null) menu_dropdown_hide(window.touched);
+                    window.touched = item;
+                    menu_dropdown_show(item);
+                }
+                else {
+                    window.touched = null;
+                    menu_dropdown_hide(item);
+                }
+            }
+
+            window.touched_start = 0;
+
+        });
+
+    });
+
+
+    document.querySelector('.navbar-toggler').addEventListener('touchend', function() {
+        if (window.touched != null) {
+            menu_dropdown_hide(window.touched);
+            window.touched = null;
+        }
+    });
+
 });
+
+function menu_dropdown_show(item) {
+    item.classList.add('show');
+    item.querySelector(':scope > a').setAttribute('aria-expanded', 'true');
+    item.querySelector(':scope > ul.dropdown-menu').classList.add('show');
+}
+function menu_dropdown_hide(item) {
+    item.classList.remove('show');
+    item.querySelector(':scope > a').setAttribute('aria-expanded', 'false');
+    item.querySelector(':scope > ul.dropdown-menu').classList.remove('show');
+}
